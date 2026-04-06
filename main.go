37a8f702186185da3e0f75588a8965e55be652d4
youniube <francecionecbyjdnr@gmail.com>
2026-04-06 20:23:46 +0800

    2.4

--- main.go
+++ main.go
@@ -2,19 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"os"
-	"os/signal"
-	"runtime"
-	"strings"
-	"syscall"
-
-	grpcServer "github.com/xxf098/lite-proxy/api/rpc/liteserver"
-	C "github.com/xxf098/lite-proxy/constant"
-	"github.com/xxf098/lite-proxy/core"
-	"github.com/xxf098/lite-proxy/utils"
-	webServer "github.com/xxf098/lite-proxy/web"
 )
 
 var (
@@ -32,67 +20,7 @@ var (
 
 func main() {
 	flag.Parse()
-	if *version {
-		fmt.Printf("LiteSpeedTest %s %s %s with %s %s\n", C.Version, runtime.GOOS, runtime.GOARCH, runtime.Version(), C.BuildTime)
-		return
-	}
-
-	link := ""
-	for _, arg := range os.Args {
-		if strings.HasPrefix(arg, "-") {
-			continue
-		}
-		if _, err := utils.CheckLink(arg); err == nil {
-			link = arg
-			break
-		}
-	}
-
-	if *test != "" {
-		if err := webServer.TestFromCMD(*test, conf); err != nil {
-			log.Fatal(err)
-		}
-		return
-	}
-
-	if *grpc {
-		if err := grpcServer.StartServer(uint16(*port)); err != nil {
-			log.Fatalln(err)
-		}
-		return
-	}
-
-	if link == "" {
-		if len(os.Args) < 2 {
-			*port = 10888
-		}
-		if err := webServer.ServeFile(*port); err != nil {
-			log.Fatalln(err)
-		}
-		return
-	}
-
-	c := core.Config{
-		LocalHost:      "0.0.0.0",
-		LocalPort:      *port,
-		Link:           link,
-		Ping:           *ping,
-		Engine:         *engineName,
-		SingboxBin:     *singboxBin,
-		SingboxWorkDir: *singboxWorkDir,
-		KeepTempFile:   *keepTemp,
-	}
-	p, err := core.StartInstance(c)
-	if err != nil {
+	if err := run(); err != nil {
 		log.Fatalln(err)
 	}
-
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
-	defer signal.Stop(sigs)
-	go func() {
-		<-sigs
-		p.Close()
-	}()
-	p.Run()
 }
